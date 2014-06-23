ad0e7e28ce176360715d87eff823a091704cfc3e
Richard Deloge <r.deloge@uni-alteri.com>
2014-06-24 00:15:04 +0200

    prepare console helper

---
+++ lib/UniAlteri/States/Command/ClassCreate.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * States
+ *
+ * LICENSE
+ *
+ * This source file is subject to the MIT license and the version 3 of the GPL3
+ * license that are bundled with this package in the folder licences
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to contact@uni-alteri.com so we can send you a copy immediately.
+ *
+ * @package     States
+ * @subpackage  Command
+ * @copyright   Copyright (c) 2009-2014 Uni Alteri (http://agence.net.ua)
+ * @link        http://teknoo.it/states Project website
+ * @license     http://teknoo.it/states/license/mit         MIT License
+ * @license     http://teknoo.it/states/license/gpl-3.0     GPL v3 License
+ * @author      Richard Déloge <r.deloge@uni-alteri.com>
+ * @version     0.9.2
+ */
+
+namespace UniAlteri\States\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class ClassCreate
+ * Command to create a new empty stated class
+ *
+ * @package     States
+ * @subpackage  Command
+ * @copyright   Copyright (c) 2009-2014 Uni Alteri (http://agence.net.ua)
+ * @link        http://teknoo.it/states Project website
+ * @license     http://teknoo.it/states/license/mit         MIT License
+ * @license     http://teknoo.it/states/license/gpl-3.0     GPL v3 License
+ * @author      Richard Déloge <r.deloge@uni-alteri.com>
+ */
+ class ClassCreate extends Command
+ {
+     /**
+      * Configures the current command.
+      */
+     protected function configure()
+     {
+         $this->setName('class:create')
+            ->setDescription('Create a new empty stated class')
+            ->addArgument(
+                'name',
+                 InputArgument::REQUIRED,
+                 'Full qualified name of the new stated class'
+             )
+            ->addOption(
+                'path',
+                 'p',
+                 InputOption::VALUE_NONE,
+                 'Path where localise the new stated class'
+             );
+     }
+
+     /**
+      * Executes the current command.
+      *
+      * This method is not abstract because you can use this class
+      * as a concrete class. In this case, instead of defining the
+      * execute() method, you set the code to execute by passing
+      * a Closure to the setCode() method.
+      *
+      * @param InputInterface  $input  An InputInterface instance
+      * @param OutputInterface $output An OutputInterface instance
+      *
+      * @return null|int     null or 0 if everything went fine, or an error code
+      *
+      * @throws \LogicException When this abstract method is not implemented
+      * @see    setCode()
+      */
+     protected function execute(InputInterface $input, OutputInterface $output)
+     {
+     }
+ }
---
+++ lib/UniAlteri/States/Command/ClassMigrateIntegrated.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * States
+ *
+ * LICENSE
+ *
+ * This source file is subject to the MIT license and the version 3 of the GPL3
+ * license that are bundled with this package in the folder licences
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to contact@uni-alteri.com so we can send you a copy immediately.
+ *
+ * @package     States
+ * @subpackage  Command
+ * @copyright   Copyright (c) 2009-2014 Uni Alteri (http://agence.net.ua)
+ * @link        http://teknoo.it/states Project website
+ * @license     http://teknoo.it/states/license/mit         MIT License
+ * @license     http://teknoo.it/states/license/gpl-3.0     GPL v3 License
+ * @author      Richard Déloge <r.deloge@uni-alteri.com>
+ * @version     0.9.2
+ */
+
+namespace UniAlteri\States\Command;
+
+use Symfony\Component\Console\Command\Command;
+
+/**
+ * Class ClassMigrateIntegrated
+ * Command to migrate a standard stated class to integrated implementation
+ *
+ * @package     States
+ * @subpackage  Command
+ * @copyright   Copyright (c) 2009-2014 Uni Alteri (http://agence.net.ua)
+ * @link        http://teknoo.it/states Project website
+ * @license     http://teknoo.it/states/license/mit         MIT License
+ * @license     http://teknoo.it/states/license/gpl-3.0     GPL v3 License
+ * @author      Richard Déloge <r.deloge@uni-alteri.com>
+ */
+class ClassMigrateIntegrated extends Command
+{
+    /**
+     * Configures the current command.
+     */
+    protected function configure()
+    {
+        $this->setName('class:migrate:integrated')
+            ->setDescription('Migrate a standard stated class to integrated implementation')
+            ->addOption(
+                'path',
+                'p',
+                InputOption::VALUE_NONE,
+                'Path of the stated class to migrate'
+            );
+    }
+
+    /**
+     * Executes the current command.
+     *
+     * This method is not abstract because you can use this class
+     * as a concrete class. In this case, instead of defining the
+     * execute() method, you set the code to execute by passing
+     * a Closure to the setCode() method.
+     *
+     * @param InputInterface  $input  An InputInterface instance
+     * @param OutputInterface $output An OutputInterface instance
+     *
+     * @return null|int     null or 0 if everything went fine, or an error code
+     *
+     * @throws \LogicException When this abstract method is not implemented
+     * @see    setCode()
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+    }
+}
