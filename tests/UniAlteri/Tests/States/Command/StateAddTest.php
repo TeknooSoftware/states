4d93e73701f40a27dd0bce8ec8454e91a5216d81
Richard Deloge <r.deloge@uni-alteri.com>
2015-01-14 22:45:49 +0100

    writing test

--- tests/UniAlteri/Tests/States/Command/StateAddTest.php
+++ tests/UniAlteri/Tests/States/Command/StateAddTest.php
@@ -21,6 +21,9 @@
 
 namespace UniAlteri\Tests\States\Command;
 
+use UniAlteri\States\Command\StateAdd;
+use UniAlteri\States\Command\Writer\State;
+
 /**
  * Class StateAddTest
  *
@@ -34,5 +37,81 @@ namespace UniAlteri\Tests\States\Command;
  */
 class StateAddTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var State
+     */
+    protected $state;
 
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject|State
+     */
+    protected function buildStateMock()
+    {
+        if (!$this->state instanceof \PHPUnit_Framework_MockObject_MockObject) {
+            $this->state = $this->getMock(
+                'UniAlteri\States\Command\Writer\State',
+                array(),
+                array(),
+                '',
+                false
+            );
+        }
+
+        return $this->state;
+    }
+
+    /**
+     * @return StateAdd
+     */
+    protected function buildCommand()
+    {
+        return new StateAdd(
+            null,
+            function($service, $destinationPath) {
+                switch ($service) {
+                    case 'Writer\State':
+                        return $this->buildStateMock();
+                        break;
+                    default:
+                        $this->fail('Error, bad service called');
+                        break;
+                }
+            }
+        );
+    }
+
+    public function testCreateState()
+    {
+        $input = $this->getMock('Symfony\Component\Console\Input\InputInterface', [], [], '', false);
+        $input->expects($this->any())
+            ->method('getArgument')
+            ->willReturnMap(
+                [
+                    ['className', '\\vendor\\project\\package\\fooBar'],
+                    ['name', 'newStateClass']
+                ]
+            );
+
+        $input->expects($this->any())
+            ->method('getOption')
+            ->willReturnMap(
+                [
+                    ['path', 'path/to/class']
+                ]
+            );
+
+        $output = $this->getMock('Symfony\Component\Console\Output\OutputInterface', [], [], '', false);
+
+        $this->buildStateMock()
+            ->expects($this->once())
+            ->method('createState')
+            ->with(
+                $this->equalTo('fooBar'),
+                $this->equalTo('\\vendor\\project\\package'),
+                $this->equalTo('newStateClass')
+            );
+
+        $command = $this->buildCommand();
+        $command->run($input, $output);
+    }
 }
