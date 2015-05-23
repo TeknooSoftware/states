59787853e59a1b5901cc674eb6010870e742ed0e
Richard Deloge <r.deloge@uni-alteri.com>
2015-05-23 22:45:32 +0200

    remove useless test

--- tests/UniAlteri/Tests/States/States/AbstractStatesTest.php
+++ tests/UniAlteri/Tests/States/States/AbstractStatesTest.php
@@ -78,21 +78,6 @@ abstract class AbstractStatesTest extends \PHPUnit_Framework_TestCase
     abstract protected function getMockProxy();
 
     /**
-     * Test exception when the Container is not valid when we set a bad object as di container.
-     */
-    public function testSetDiContainerBad()
-    {
-        $object = $this->getPublicClassObject(false);
-        try {
-            $object->setDIContainer(new \DateTime());
-        } catch (\Exception $e) {
-            return;
-        }
-
-        $this->fail('Error, the object must throw an exception when the DI Container is not valid');
-    }
-
-    /**
      * Test behavior for methods Set And GetDiContainer.
      */
     public function testSetAndGetDiContainer()
