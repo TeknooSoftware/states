42d5218f85e3ba5803fb707997aa81095928bc85
Richard Deloge <r.deloge@uni-alteri.com>
2015-05-23 22:35:30 +0200

    remove useless test

--- tests/UniAlteri/Tests/States/Loader/FinderStandardTest.php
+++ tests/UniAlteri/Tests/States/Loader/FinderStandardTest.php
@@ -127,21 +127,6 @@ class FinderStandardTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * Test exception when the Container is not valid when we set a bad object as di container.
-     */
-    public function testSetDiContainerBad()
-    {
-        $object = new Loader\FinderStandard('', '');
-        try {
-            $object->setDIContainer(new \DateTime());
-        } catch (\Exception $e) {
-            return;
-        }
-
-        $this->fail('Error, the object must throw an exception when the DI Container is not valid');
-    }
-
-    /**
      * Test behavior for methods Set And GetDiContainer.
      */
     public function testSetAndGetDiContainer()
