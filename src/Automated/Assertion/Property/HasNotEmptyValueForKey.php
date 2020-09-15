338957edbce7c01cd3cc4d79714cde09bc833435
Richard Deloge <richarddeloge@gmail.com>
2020-09-15 13:21:21 +0200

    add more assertions checks for automation wip

---
+++ src/Automated/Assertion/Property/HasNotEmptyValueForKey.php
@@ -0,0 +1,63 @@
+<?php
+
+/*
+ * States.
+ *
+ * LICENSE
+ *
+ * This source file is subject to the MIT license and the version 3 of the GPL3
+ * license that are bundled with this package in the folder licences
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to richarddeloge@gmail.com so we can send you a copy immediately.
+ *
+ *
+ * @copyright   Copyright (c) 2009-2020 Richard Déloge (richarddeloge@gmail.com)
+ *
+ * @link        http://teknoo.software/states Project website
+ *
+ * @license     http://teknoo.software/license/mit         MIT License
+ * @author      Richard Déloge <richarddeloge@gmail.com>
+ */
+
+declare(strict_types=1);
+
+namespace Teknoo\States\Automated\Assertion\Property;
+
+use Teknoo\Immutable\ImmutableTrait;
+
+/**
+ * Constraint to use with Teknoo\States\Automated\Property to check if a property is an array and has a required key
+ *
+ * @copyright   Copyright (c) 2009-2020 Richard Déloge (richarddeloge@gmail.com)
+ *
+ * @link        http://teknoo.software/states Project website
+ *
+ * @license     http://teknoo.software/license/mit         MIT License
+ * @author      Richard Déloge <richarddeloge@gmail.com>
+ */
+class HasNotEmptyValueForKey extends AbstractConstraint
+{
+    use ImmutableTrait;
+
+    private string $keyName;
+
+  public function __construct(string $keyName)
+  {
+    $this->uniqueConstructorCheck();
+
+    $this->keyName = $keyName;
+  }
+
+  /**
+     * {@inheritdoc}
+     */
+    public function check(&$value): ConstraintInterface
+    {
+        if (\is_array($value) && !empty($value[$this->keyName])) {
+            $this->isValid($value);
+        }
+
+        return $this;
+    }
+}
---
+++ src/Automated/Assertion/Property/IsScalar.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * States.
+ *
+ * LICENSE
+ *
+ * This source file is subject to the MIT license and the version 3 of the GPL3
+ * license that are bundled with this package in the folder licences
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to richarddeloge@gmail.com so we can send you a copy immediately.
+ *
+ *
+ * @copyright   Copyright (c) 2009-2020 Richard Déloge (richarddeloge@gmail.com)
+ *
+ * @link        http://teknoo.software/states Project website
+ *
+ * @license     http://teknoo.software/license/mit         MIT License
+ * @author      Richard Déloge <richarddeloge@gmail.com>
+ */
+
+declare(strict_types=1);
+
+namespace Teknoo\States\Automated\Assertion\Property;
+
+use Teknoo\Immutable\ImmutableTrait;
+
+/**
+ * Constraint to use with Teknoo\States\Automated\Property to check if a property is a scalar
+ *
+ * @copyright   Copyright (c) 2009-2020 Richard Déloge (richarddeloge@gmail.com)
+ *
+ * @link        http://teknoo.software/states Project website
+ *
+ * @license     http://teknoo.software/license/mit         MIT License
+ * @author      Richard Déloge <richarddeloge@gmail.com>
+ */
+class IsScalar extends AbstractConstraint
+{
+    use ImmutableTrait;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function check(&$value): ConstraintInterface
+    {
+        if (\is_scalar($value)) {
+            $this->isValid($value);
+        }
+
+        return $this;
+    }
+}
